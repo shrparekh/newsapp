b3f1c1eb1841e96efc9e1929ac4aacec393304e5
shrey parekh <your_email@example.com>
2024-04-01 18:07:42 +0530

    fetch data using api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageurl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageurl
+                ? "https:th.bing.com/th/id/OIP.Cl56H6WgxJ8npVqyhefTdQHaHa?w=203&h=203&c=7&r=0&o=5&pid=1.7"
+                : imageurl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title"> {title}</h5>
             <p className="card-text">{discription}</p>
