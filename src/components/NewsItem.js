5e241fbdd8229326a81237c51a0f3e25be70cbcd
shrey parekh <your_email@example.com>
2024-04-01 17:14:30 +0530

    made the api key ,create the card component for the news to display

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, discription } = this.props;
+    let { title, discription, imageurl, newsurl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageurl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title"> {title}</h5>
             <p className="card-text">{discription}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a
+              href={newsurl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
             </a>
           </div>
         </div>
